2f345fa83264c9c04f14c5510b9cb3bce42cda3c
prajwal.gowda@ext.dunzo.in <prajwalg@testvagrant.com>
2023-06-24 08:01:01 +0530

    parameterized to take volume path

--- docker-compose-entrypoint.sh
+++ docker-compose-entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # Check if the number of arguments is correct
-if [ $# -ne 2 ]; then
-    echo "Usage: ./create_env.sh <command1> <command2>"
+if [ $# -ne 3 ]; then
+    echo "Usage: ./create_env.sh <maven command to run the test in container 1>
+                   <maven command to run the test in container 2>
+                   <path to the volume to be mounted>"
     exit 1
 fi
 
 # Assign the arguments to variables
 Replica1="$1"
 Replica2="$2"
+VolumePath="$3"
 
 # Create the .env file
 echo "Replica1=$Replica1" > .env
@@ -17,4 +20,7 @@ echo "Replica2=$Replica2" >> .env
 # Success message
 echo "The .env file has been created successfully."
 
-docker-compose up
+# Modify the Docker Compose file to include the variable directly
+export DOCKER_VOLUME_PATH="$VolumePath"
+
+docker-compose up -d
--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -8,12 +8,24 @@ public class Runner {
     @Test(groups = "test1")
     void test1() {
         System.out.println("Hello World");
-        assertThat("Yes", 1,equalTo(1));
+        assertThat("Yes", 1, equalTo(1));
     }
 
     @Test(groups = "test2")
     void test2() {
         System.out.println("Hello World");
-        assertThat("Yes", 1,equalTo(2));
+        assertThat("Yes", 1, equalTo(1));
+    }
+
+    @Test(groups = "test3")
+    void test3() {
+        System.out.println("Hello World3");
+        assertThat("Yes", 1, equalTo(1));
+    }
+
+    @Test(groups = "test4")
+    void test4() {
+        System.out.println("Hello World4");
+        assertThat("Yes", 1, equalTo(1));
     }
 }

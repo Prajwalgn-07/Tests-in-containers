f629e1164a7e02bef714093d3b83174bb8118a23
prajwal.gowda@ext.dunzo.in <prajwalg@testvagrant.com>
2023-06-25 18:44:32 +0530

    allure_docker_service --------------------- 1. used allure docker service to store multiple container result 2. refactored shell script to remove volume path as we are using named volume now

--- docker-compose-entrypoint.sh
+++ docker-compose-entrypoint.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
 # Check if the number of arguments is correct
-if [ $# -ne 3 ]; then
-    echo "Usage: ./create_env.sh <maven command to run the test in container 1>
-                   <maven command to run the test in container 2>
-                   <path to the volume to be mounted>"
+if [ $# -ne 2 ]; then
+    echo "Usage: ./create_env.sh <groups you want to run on container 1>
+                   <groups you want to run on container 2>"
     exit 1
 fi
 
+# remove the contents of allure-results directory
+rm -rf allure-results/*
+
 # Assign the arguments to variables
-Replica1="$1"
-Replica2="$2"
-VolumePath="$3"
+Replica1="mvn test $1"
+Replica2="mvn test $2"
 
 # Create the .env file
-echo "Replica1=$Replica1" > .env
+echo "Replica1=$Replica1" >> .env
 echo "Replica2=$Replica2" >> .env
 
 # Success message
 echo "The .env file has been created successfully."
 
-# Modify the Docker Compose file to include the variable directly
-export DOCKER_VOLUME_PATH="$VolumePath"
-
-docker-compose up -d
+docker-compose up
